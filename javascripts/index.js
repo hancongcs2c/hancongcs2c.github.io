37295c4242ee14af4260802d9b5d691597bd90c0
insekkei <insekkei@yahoo.com>
2014-09-02 18:01:16 +0800

    add index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,5 +1,13 @@
 $(function(){
-if($('section').hasClass('all'))
-        $('.all').load('all.html');
+//	if($('section').hasClass('all'))
+//		$('.all').load('all.html');
+	$('#index-nav li a').hover(function(e){
+		var $thisLink = $(this);
+		if($thisLink.attr('href').match('undefined')!='undefined'){
+			$thisLink.addClass('scale1').addClass('transition').mouseout(function(e){
+				$(this).removeClass('scale1');
+			});
+		}
+	});
 })
 
