63086d9c325a8f6faa2d459b606071042f3a600a
insekkei <insekkei@yahoo.com>
2015-05-28 12:04:24 +0800

    ..

--- js/index.js
+++ js/index.js
@@ -15,12 +15,38 @@ $(function(){
 		e.preventDefault();
 
 	});
-	// 事件函数方式  
-	$(window).scroll(function(e) {  
-	    console.log($('body').css('scrollTop'));  
-	}); 
+
 	$('p.more').click(function(e){
 		$('div.more').toggleClass('collapse');
 	});
+
+    $(window).scroll(function(){
+
+    	var toTop = $(document).scrollTop();
+
+    	var pro1 = $('#works').offset().top,
+    		pro2 = $('#doodles').offset().top,
+    		pro3 = $('#skills').offset().top,
+    		pro4 = $('#about').offset().top;
+
+    	//deside witch is the current link
+    	if(toTop < pro1){
+    		$('#nav-list li').eq(0).addClass("current").siblings().removeClass("current");
+    		location.hash = '#index';
+    	}else if(toTop < pro2){
+    		$('#nav-list li').eq(1).addClass("current").siblings().removeClass("current");
+    		location.hash = '#works';
+    	}else if(toTop < pro3){
+    		$('#nav-list li').eq(2).addClass("current").siblings().removeClass("current");
+    		location.hash = '#doodles';
+    	}else if(toTop < pro4){
+    		$('#nav-list li').eq(3).addClass("current").siblings().removeClass("current");
+    		location.hash = '#skills';
+    	}else{
+    		$('#nav-list li').eq(4).addClass("current").siblings().removeClass("current");
+    		location.hash = '#about';
+    	}
+    	
+    });
 
 });
